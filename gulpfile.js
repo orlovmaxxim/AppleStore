a9da403ef2135b3204bc8f60de2ace896c94228c
maxim <orlov.maxxim@gmail.com>
2015-08-13 13:09:42 +0300

    added skeleton html page and img folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 var gulp = require('gulp'),
+		jade = require('gulp-jade'),
 		concatCss = require('gulp-concat-css'),
 		rename = require('gulp-rename'),
 		notify = require('gulp-notify'),
 		liveReload = require('gulp-livereload'),
 		browserSync = require('browser-sync'),
 		connect = require('gulp-connect'),
-		minifyCss = require('gulp-minify-css');
+		minifyCss = require('gulp-minify-css'),
+		reload = browserSync.reload;
+
+/*ЛОКАЛЬНАЯ РАЗРАБОТКА*/
+
+//task jade
+
+gulp.task('jade', function(){
+	gulp.src('src/templates/pages/*.jade')
+	.pipe(jade())
+	.on('error', log)
+	.pipe(gulp.dest('src/'))
+	.pipe(reload({stream: true}));
+});
 
 //task connect - таск запуска локального сервера
-
-gulp.task('connect', function(){
+//локальный сервер запускается, но только после компиляции jade (см. второй параметр)
+gulp.task('connect', ['jade'], function(){
 	browserSync({
+		notify: false,
 		port: 9000,
 		server: {
 			baseDir: 'src'
@@ -31,14 +46,38 @@ gulp.task('css', function () {
 
  
 
-//отслеживание таска - слежка и автоматический запуск gulp
+//отслеживание таска - слежка и автоматический запуск gulp задач
 gulp.task('watch', function(){
+	gulp.watch('src/templates/**/*.jade', ['jade']);
 	gulp.watch([
-		'src/*.html',
+		/*'src/*.html',*/
 		'src/js/**/*.js',
 		'src/css/**/*.css'
-		]).on('change', browserSync.reload);
+		]).on('change', reload);
 });
  
 // default task - таск по умолчанию
 gulp.task('default', ['connect','watch']);
+
+
+
+/*ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ*/
+
+//Наглядный вывод ошибок в консоль
+
+var log = function(error) {
+	console.log([
+		'',
+		"<===ERROR MESSAGE START===>",
+		("[" + error.name + " in " + error.plugin + "]"),
+		error.message,
+		"<===ERROR MESSAGE END===>",
+		''
+		].join('\n'));
+		this.end;
+}
+
+
+/*gulp.task(name, deps, fn)
+	deps - массив задач, которые будут выполнены ДО запуска задачи name
+*/
